3f9f925a2200d4a7d0171dec8e7acf2f7f8c27ed
Trollope91 <ljtdev91@gmail.com>
2023-06-06 13:06:41 +0000

    Add how to play modal and page load js trigger

--- assets/js/app.js
+++ assets/js/app.js
@@ -78,3 +78,11 @@ function restartGame() {
 
 shuffle();
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+window.addEventListener('DOMContentLoaded', function() {
+  setTimeout(function() {
+    var modal = document.querySelector('.modal');
+    var modalInstance = new bootstrap.Modal(modal);
+    modalInstance.show();
+  }, 1000);
+});
