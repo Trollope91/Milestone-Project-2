e08a69fabfbd50a566a6e3ab2f7262e604e04edf
Trollope91 <ljtdev91@gmail.com>
2023-06-06 08:11:08 +0000

    Add match check logic and return flip

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,8 +11,32 @@ function flipCard() {
   if (!hasFlippedCard) {
     hasFlippedCard = true;
     firstCard = this;
+    return;
   }
+
+  secondCard = this;
+  hasFlippedCard = false;
+
+  checkForMatch();
 }
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+  }, 1500);
+}
+
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
