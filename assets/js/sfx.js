a0186cab783b5b960482f2a816eb1f1f69d88c94
Trollope91 <ljtdev91@gmail.com>
2023-06-08 17:57:05 +0000

    Add complete sound effects java and html

--- assets/js/sfx.js
+++ assets/js/sfx.js
@@ -1,2 +1,17 @@
 var bulb = new Audio()
-bulb.src = "/assets/images/bulbasaur.mp3"
+bulb.src = "/assets/sfx/bulbasaur.mp3"
+
+var char = new Audio()
+char.src = "/assets/sfx/charmander.mp3"
+
+var squir = new Audio()
+squir.src = "/assets/sfx/squirtle.mp3"
+
+var haunt = new Audio()
+haunt.src = "/assets/sfx/haunter.mp3"
+
+var mew = new Audio()
+mew.src = "/assets/sfx/mewtwo.mp3"
+
+var pika = new Audio()
+pika.src = "/assets/sfx/pikachu.mp3"
