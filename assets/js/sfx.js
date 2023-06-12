7087f4cd4612773fa008fcbf330e1d021bbcef23
Trollope91 <ljtdev91@gmail.com>
2023-06-12 10:12:05 +0000

    Change audio filepaths in sfx.js

--- assets/js/sfx.js
+++ assets/js/sfx.js
@@ -1,29 +1,29 @@
 var bulb = new Audio()
-bulb.src = "/assets/sfx/bulbasaur.mp3"
+bulb.src = "./assets/sfx/bulbasaur.mp3"
 
 var char = new Audio()
-char.src = "/assets/sfx/charmander.mp3"
+char.src = "./assets/sfx/charmander.mp3"
 
 var squir = new Audio()
-squir.src = "/assets/sfx/squirtle.mp3"
+squir.src = "./assets/sfx/squirtle.mp3"
 
 var haunt = new Audio()
-haunt.src = "/assets/sfx/haunter.mp3"
+haunt.src = "./assets/sfx/haunter.mp3"
 
 var mew = new Audio()
-mew.src = "/assets/sfx/mewtwo.mp3"
+mew.src = "./assets/sfx/mewtwo.mp3"
 
 var pika = new Audio()
-pika.src = "/assets/sfx/pikachu.mp3"
+pika.src = "./assets/sfx/pikachu.mp3"
 
 var bgmusic = new Audio()
-bgmusic.src = "/assets/sfx/gamecorner.mp3"
+bgmusic.src = "./assets/sfx/gamecorner.mp3"
 
 var butter = new Audio()
-butter.src = "/assets/sfx/butterfree.mp3"
+butter.src = "./assets/sfx/butterfree.mp3"
 
 var nido = new Audio()
-nido.src = "/assets/sfx/nidoking.mp3"
+nido.src = "./assets/sfx/nidoking.mp3"
 
 function toggleAudio() {
     if (bgmusic.paused) {
